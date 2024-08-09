6bf0928fba77b101d74ca7aca5d2630da395c1ea
aybeniz <aybenizkazimova15@gmail.com>
2024-08-09 20:46:36 +0400

    USer DTO and Entity

--- src/main/java/az/edu/turing/usermanagmentsystem/model/dto/UserDto.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/dto/UserDto.java
@@ -1,4 +1,28 @@
 package az.edu.turing.usermanagmentsystem.model.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class UserDto {
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime dateOfBirth;
+
+    private boolean gender;
+
 }
--- src/main/java/az/edu/turing/usermanagmentsystem/model/entity/UserEntity.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/entity/UserEntity.java
@@ -1,4 +1,35 @@
 package az.edu.turing.usermanagmentsystem.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column( nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private LocalDateTime dateOfBirth;
+
+    private boolean gender;
+
+
 }
