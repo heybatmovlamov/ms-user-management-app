f5375be86599ad743e1bc69252afa170254f64e9
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-08-09 20:46:15 +0400

    Profile Dto and Entity changes

--- src/main/java/az/edu/turing/usermanagmentsystem/repository/ProfileRepository.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/repository/ProfileRepository.java
@@ -1,4 +1,9 @@
 package az.edu.turing.usermanagmentsystem.repository;
 
-public class ProfileRepository {
+import az.edu.turing.usermanagmentsystem.model.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, UUID> {
 }
