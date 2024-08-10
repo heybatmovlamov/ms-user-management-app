a35b87f50d71a0335ded7345afab3faaabe34ea1
aybeniz <aybenizkazimova15@gmail.com>
2024-08-10 16:16:07 +0400

    User and Profile Dto and Mapper added

--- src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
@@ -1,5 +1,34 @@
 package az.edu.turing.usermanagmentsystem.model.dto;
 
+import az.edu.turing.usermanagmentsystem.model.entity.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ProfileDto {
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Enum status;
+
+    private String username;
+
+    private byte[] profilePhoto;
+
+    private String description;
+
+    private LocalDateTime lastSeenTime;
+
 
 }
