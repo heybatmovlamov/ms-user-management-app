8c5db14d4ceb270795d476f52a6182f2cd6934a0
aybeniz <aybenizkazimova15@gmail.com>
2024-08-10 19:03:58 +0400

    Implement CRUD operations in ProfileService

--- src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
@@ -5,10 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileDto {
+    private UUID id;
 
     private LocalDateTime createdAt;
 
