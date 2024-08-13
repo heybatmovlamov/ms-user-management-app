743512704a14c2a4fa1c0d8c713ee319969cc79e
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-08-13 14:07:32 +0400

    UserDto validate

--- src/main/java/az/edu/turing/usermanagmentsystem/model/dto/UserDto.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/dto/UserDto.java
@@ -1,5 +1,6 @@
 package az.edu.turing.usermanagmentsystem.model.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,20 +12,31 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class UserDto {
 
+    @Past
     private LocalDateTime createdAt;
 
+    @Past
     private LocalDateTime updatedAt;
 
+    @NotBlank
+    @Size(min = 2 ,max = 50)
     private String name;
 
+    @NotBlank
+    @Size(min = 2 , max = 50)
     private String surname;
 
+    @Email
     private String email;
 
+    @NotBlank
+    @Pattern(regexp = "\\+?[0-9]{7,15}")
     private String phoneNumber;
 
+    @Past
     private LocalDateTime dateOfBirth;
 
+    @NotNull
     private boolean gender;
 
 }
