f191dc36afccfde1e8dd562c8fc43bc0a595e4fe
aybeniz <aybenizkazimova15@gmail.com>
2024-08-13 14:44:57 +0400

    ProfileDto validation

--- src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
+++ src/main/java/az/edu/turing/usermanagmentsystem/model/dto/ProfileDto.java
@@ -1,6 +1,10 @@
 package az.edu.turing.usermanagmentsystem.model.dto;
 
 import az.edu.turing.usermanagmentsystem.model.enums.ProfileStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +18,26 @@ import java.util.UUID;
 public class ProfileDto {
     private UUID id;
 
+    @NotNull
     private LocalDateTime createdAt;
 
+    @NotNull
     private LocalDateTime updatedAt;
-
+    @NotNull
     private ProfileStatus status;
 
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String username;
 
+    @NotNull
     private byte[] profilePhoto;
 
+    @NotNull
+    @Size(min = 2, max = 50)
     private String description;
 
+    @Past
     private LocalDateTime lastSeenTime;
 
 
